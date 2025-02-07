e1f7625dbff40c5cc8e71440e84aab5dabab2a89
PHILIPWins <80891294+PHILIPWins@users.noreply.github.com>
2025-02-07 11:59:34 +0100

    Add files via upload

---
+++ CKA.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import numpy as np
+import HSIC
+import math
+def CKA(time, space):
+    weights_time_2d = np.squeeze(time)
+    V = weights_time_2d[:,0].reshape(-1,1)
+    subset_space = space[:,0,:,:]
+    space_weights_2d = np.squeeze(subset_space)
+    W = space_weights_2d[:,0].reshape(-1,1)
+    K = np.matmul(V,V.T)
+    L = np.matmul(W,W.T) 
+    HSIC_K_K = HSIC.hsic_gam(K,K)
+    HSIC_K_L = HSIC.hsic_gam(K,L)
+    HSIC_L_L = HSIC.hsic_gam(L,L)
+
+    CKA = HSIC_K_L[0]/(math.sqrt(HSIC_K_K[0]*HSIC_L_L[0])) ### Tuple, took [0] since (test-statistic, test-threshold for alpha)
+    return CKA
+
+
