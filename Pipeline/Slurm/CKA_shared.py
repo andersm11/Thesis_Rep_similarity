ab5d672222ce8e823b6002f13bfa00901396af51
andersm11 <79901566+andersm11@users.noreply.github.com>
2025-05-12 13:49:42 +0200

    cka for temp_share is bad

--- Pipeline/Slurm/CKA_shared.py
+++ Pipeline/Slurm/CKA_shared.py
@@ -8,12 +8,11 @@ import os
 from CKA_functions import compute_all_model_kernels_indexed
 from CKA_functions import compute_all_model_CKA_lowmem
 import shutil
-
-model_direc = "models"
-activation_direc = "activations"
-kernel_direc = "kernels"
+model_direc = "cka_models"
+activation_direc = "activations2"
+kernel_direc = "kernels2"
 keyfile_folder = "Shared_Keys"
-cka_folder = "cka_results"
+cka_folder = "cka_results2"
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda and torch.cuda.is_available() else "cpu")
@@ -23,15 +22,15 @@ X = torch.load('FACED_dataset/emotion_test_set.pt')
 X = [sublist[0] for sublist in X]  # Extract the first tensor in each tuple
 X = torch.stack(X).to(device)     # Stack and move to device
 
-layer_names = ["fc"]
+layer_names = ["spatial","sgconv","spatial_att"]
 batch_size = 288
-n_batches = 10
+n_batches = 6
 
 # Iterate over all CSV key files in the keyfile directory
 for file in os.listdir(keyfile_folder):
     if file.endswith(".csv") and file.startswith("Shared_Keys_"):
-        keyfile_path = os.path.join(keyfile_folder, file)
-        print(f"Computing kernels for: {file}", flush=True)
+        keyfile_path = os.path.join(keyfile_folder, "Shared_Keys_Spatial.csv")
+        print(f"Computing kernels for: {keyfile_path}", flush=True)
 
         compute_all_model_kernels_indexed(
             model_direc,
--- Pipeline/python_models/CKA_pipeline.py
+++ Pipeline/python_models/CKA_pipeline.py
@@ -1,3 +1,3 @@
-from CKA_functions import compose_heat_matrix_acc,compose_heat_matrix_shared
-compose_heat_matrix_shared("../cka_results","cka_heatmaps","../Shared_Keys","CKA heatmap_shared_fc_temporalll")
-#compose_heat_matrix_acc("../cka_results","cka_heatmaps","../models","CKA heatmap_shared_fc_all")
+from CKA_functions import compose_heat_matrix_acc,compose_heat_matrix_shared_full,compose_heat_matrix_shared
+compose_heat_matrix_shared("../cka_results","cka_heatmaps","../Shared_Keys","CKA heatmap_shared_layers_spatial")
+#compose_heat_matrix_acc("../cka_results","cka_heatmaps","../models","CKA heatmap_spatial")
